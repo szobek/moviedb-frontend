d638ed49bc27e5e8a77918a101f8e9e57b0806b8
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-07-08 16:44:36 +0200

    feat(nav): add navigation component with toggle functionality and routing feat(app): integrate navigation component into app layout feat(index): include Bootstrap CSS for styling refactor(movie-description): remove back button from movie description

---
+++ src/app/core/nav/nav.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss'
+})
+export class NavComponent {
+  navOpen:boolean = false;
+
+  toggleNav() {
+    this.navOpen = !this.navOpen;
+  }
+
+}
