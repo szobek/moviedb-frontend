e7c0f1e0608865465159f00679524b6a7eb2dd15
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-07-14 21:25:53 +0200

    feat:generate search component

---
+++ src/app/movies/components/movie-search/movie-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-search',
+  imports: [],
+  templateUrl: './movie-search.component.html',
+  styleUrl: './movie-search.component.scss'
+})
+export class MovieSearchComponent {
+
+}
--- src/app/movies/movie.routes.ts
+++ src/app/movies/movie.routes.ts
@@ -3,9 +3,11 @@ import { MovieListComponent } from './components/movie-list/movie-list.component
 import { MovieDescriptionComponent } from './components/movie-description/movie-description.component';
 import { GenresComponent } from './components/genres/genres.component';
 import { MovieByGenreComponent } from './components/movie-by-genre/movie-by-genre.component';
+import { MovieSearchComponent } from './components/movie-search/movie-search.component';
 
 export const movieRoutes: Routes = [
     { path: '', component: MovieListComponent },
+    { path: 'search', component: MovieSearchComponent },
     { path: 'genres',component:GenresComponent},
     { path: 'genres/:id',component:MovieByGenreComponent},
     { path: ':id', component:MovieDescriptionComponent},
