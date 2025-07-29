cdb1aaa21fa18b62f1d5ff59416e7f663cefa32f
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-07-29 18:25:04 +0200

    fix: update movie properties and API endpoints for consistency

--- src/app/movies/models/Movie.model.ts
+++ src/app/movies/models/Movie.model.ts
@@ -1,9 +1,11 @@
 export interface Movie {
-  movie_id: number;
+  id: number;
   title: string;
   year: number;
   rating: number;
   description: string;
   genres: Array<string>;
   actors: Array<string>;
+  posterUrl: string;
+  director:string
 }
--- src/app/movies/sercvices/call.service.ts
+++ src/app/movies/sercvices/call.service.ts
@@ -8,30 +8,30 @@ import { Genre } from '../models/Genre.model';
   providedIn: 'root'
 })
 export class CallService {
-  private readonly _BASE_URL = 'http://127.0.0.1:3000';
+  private readonly _BASE_URL = 'http://127.0.0.1:3000/api/movies';
  selectedGenre:Genre|null=null
   constructor(private readonly http: HttpClient) { }
 
   getAllMovie():Observable<Movie[]>{
-    return this.http.get<Movie[]>(`${this._BASE_URL}/movies`)
+    return this.http.get<Movie[]>(`${this._BASE_URL}/`)
   }
 
   getMovieById(id:number):Observable<Movie>{
-    return this.http.get<Movie>(`${this._BASE_URL}/movies/${id}`)
+    return this.http.get<Movie>(`${this._BASE_URL}/${id}`)
   }
   getAllGenres(){
-    return this.http.get<string[]>(`${this._BASE_URL}/movies/genres`)
+    return this.http.get<string[]>(`${this._BASE_URL}/genres`)
   }
   getMoviesByGenre(genre:string):Observable<Movie[]>{
-    return this.http.get<Movie[]>(`${this._BASE_URL}/movies/genres/${genre}`)
+    return this.http.get<Movie[]>(`${this._BASE_URL}/genres/${genre}`)
   }
   getAllActors(){
-    return this.http.get(`${this._BASE_URL}/movies/actors`)
+    return this.http.get(`${this._BASE_URL}/actors`)
   }
   searchMovies(criteria:any){
-    return this.http.post(`${this._BASE_URL}/movies/search`,criteria)
+    return this.http.get(`${this._BASE_URL}/search`)
   }
   getMoviesByActor(id:number){
-    return this.http.get(`${this._BASE_URL}/movies/actors/${id}`)
+    return this.http.get(`${this._BASE_URL}/actors/${id}`)
   }
 }
