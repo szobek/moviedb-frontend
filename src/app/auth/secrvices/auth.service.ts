5deace0a9c4f764b740acba56c6f2d8fdc02d76a
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-08-12 11:38:54 +0200

    feat: update accesstoken by refreshtoken

--- src/app/auth/secrvices/auth.service.ts
+++ src/app/auth/secrvices/auth.service.ts
@@ -4,35 +4,40 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { User } from '../models/User.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   private readonly _BASE_URL = 'http://127.0.0.1:3000/api/auth';
-  user:BehaviorSubject<User|null> = new BehaviorSubject<User|null>(null)
-  constructor(private readonly http: HttpClient) { }
+  user: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
+  constructor(private readonly http: HttpClient) {}
 
-  register(registeredUser:User):Observable<any>{
-    return this.http.post(`${this._BASE_URL}/register`,registeredUser)
+  register(registeredUser: User): Observable<any> {
+    return this.http.post(`${this._BASE_URL}/register`, registeredUser);
   }
 
-  login(user:User):Observable<any>{
-    return this.http.post(`${this._BASE_URL}/login`,user)
+  login(user: User): Observable<any> {
+    return this.http.post(`${this._BASE_URL}/login`, user);
   }
 
-  getUsers(){
-     const headers = new HttpHeaders({
-      'Authorization': 'Bearer '+this.user.value?.accessToken||''
-    });
-    return this.http.get(`${this._BASE_URL}/users`,{headers})
-
-  }
-  approveUser(user:User={id:'-1'} as User){
-    const data={
-      id:user.id
-    }
+  getUsers() {
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer '+this.user.value?.accessToken||''
+      Authorization: 'Bearer ' + this.user.value?.accessToken || '',
     });
-    return this.http.patch(`${this._BASE_URL}/approval`,data,{headers})
+    return this.http.get(`${this._BASE_URL}/users`, { headers });
+  }
+  approveUser(user: User = { id: '-1' } as User) {
+    const data = {
+      id: user.id,
+    };
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.user.value?.accessToken || '',
+    });
+    return this.http.patch(`${this._BASE_URL}/approval`, data, { headers });
+  }
+  updateAccessToken() {
+    const headers = new HttpHeaders({
+      refresh_token: this.user.value?.refreshToken || '',
+    });
+    return this.http.put(`${this._BASE_URL}/refresh-token`,null, { headers });
   }
 }
--- src/app/dashboard/components/home/home.component.ts
+++ src/app/dashboard/components/home/home.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { UserListComponent } from '../../../auth/components/user-list/user-list.component';
+import { AuthService } from '../../../auth/secrvices/auth.service';
+import { tap } from 'rxjs/operators';
+import { User } from '../../../auth/models/User.model';
 
 @Component({
   selector: 'mmdb-home',
   imports: [UserListComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  authService: AuthService = inject(AuthService);
+@Output() getUsers: EventEmitter<any>=new EventEmitter();
+  refreshToken() {
+    this.authService
+      .updateAccessToken()
+      .pipe(
+        tap((res: any) => {
+          console.log(res);
+          const user: User = this.authService.user.value as User;
+          user.accessToken = res.access_token;
+          console.log(user);
+          
+          this.authService.user.next(user);
+          localStorage.setItem('user', JSON.stringify(user));
+        })
+      )
+      .subscribe();
+  }
 }
